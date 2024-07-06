7699332bc494a49251e867dffa084f86702cf45d
vaishnavichoudhari119@gmail.com <vaishnavichoudhari119@gmail.com>
2024-07-06 20:37:34 +0530

    modified

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ function changeImage(imageName){
     const mainImage = document.getElementById('main-product-image');
     mainImage.src = imageName;
 }   
+function increment()
+{
+    const quantityElement = document.getElementById("quantity");
+    const quantity = parseInt(quantityElement.innerText);
+    quantityElement.innerText = quantity + 1;
+    if(quantity > 4){
+        alert("Quantity cannot be greater than 4");
+        quantityElement.innerText = quantity - 1;
+        return;
+    }
+}
+function decrement()
+{
+    const quantityElement = document.getElementById("quantity");
+    const quantity = parseInt(quantityElement.innerText);
+    quantityElement.innerText = quantity - 1;
+    if(quantity < 1){
+        alert("Quantity cannot be less than 1");
+        quantityElement.innerText = quantity + 1;
+        return;
+    }
+}
